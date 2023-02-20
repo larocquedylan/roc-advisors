04bf370898f4468e8437623f6822024b52537c55
Dylan L <dylancblarocque@gmail.com>
2023-02-19 21:47:10 -0700

    Configure Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import NavBar from "./components/NavBar";
 import "./index.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ValueProp from "./pages/ValueProp";
+import SolutionPage from "./pages/SolutionPage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <NavBar />
-      
-    </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/value" element={<ValueProp />} />
+        <Route path="/solutions" element={<SolutionPage />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
