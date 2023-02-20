3f5f4ed077ebf9a6c7fc8b4d04b5beb8fff751ae
Dylan L <dylancblarocque@gmail.com>
2023-02-19 21:47:42 -0700

    Finish Hero section for HomePage

---
+++ src/pages/SolutionPage.js
@@ -0,0 +1,9 @@
+function SolutionPage(){
+    return (
+        <>
+        
+        </>
+    );
+};
+
+export default SolutionPage;
---
+++ src/pages/ValueProp.js
@@ -0,0 +1,9 @@
+function ValueProp(){
+    return (
+        <>
+        
+        </>
+    );
+};
+
+export default ValueProp;
