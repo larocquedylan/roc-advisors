a5e016f84357f464dad6328a1a0c567c46497c39
Dylan L <dylancblarocque@gmail.com>
2023-02-19 21:47:52 -0700

    Finished Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer(){
+    return (
+        <div className="flex justify-between row-auto max-width-[1240px] mx-auto mt-4 py-8 px-4 text-slate bg-white">
+        <a href="mailto: justin.l@rocadvisors.ca"> justin.l@rocadvisors.ca</a>
+        <p> &copy; 2023 Roc Advisors </p>
+        <a href="tel: 780-691-5704"> 780-691-5704</a>
+        </div>
+    );
+};
+
+export default Footer;
