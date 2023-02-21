7a29e81e55a2f30bd30c6315a02fa53c6fe17cf0
Dylan L <dylancblarocque@gmail.com>
2023-02-20 18:51:45 -0700

    Add text for value proposition

--- src/pages/ValueProp.js
+++ src/pages/ValueProp.js
@@ -1,7 +1,14 @@
 function ValueProp(){
     return (
         <>
+        <p> Why: Small and medium-sized businesses in Alberta are struggling to manage their finances due to a lack of access to professional finance services, while at the same time, some businesses are facing the challenge of bloated accounting teams, which can lead to decreased capital efficiency and lower profitability. </p>
         
+        <p>What?: We offer a unique ecosystem that provides access to a team of highly engaged and knowledgeable finance professionals. Our team works with businesses to provide customized solutions that meet their unique needs, ensuring that their financials are always on track. </p>
+            
+        <p>How? Our flexible working arrangements and diverse range of expertise make us an ideal partner for businesses that need to balance their financial needs with their budget. By working with us, businesses can benefit from our collective knowledge and receive the support they need to make informed financial decisions. Our cost-effective alternative to in-house finance staff allows businesses to focus on what they do best while leaving the financial expertise to us.</p>
+        
+
+        <p> Whether you need help with bookkeeping, tax planning, or financial reporting, our team has the expertise and experience to deliver results. We pride ourselves on providing excellent customer service and building long-term relationships with our clients. When you work with us, you can trust that your business's financial health is in good hands.</p>
         </>
     );
 };
