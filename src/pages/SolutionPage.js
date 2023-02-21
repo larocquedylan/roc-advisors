46871a93c506c43a7233a142caa3ec4bfa11ea62
Dylan L <dylancblarocque@gmail.com>
2023-02-20 22:45:06 -0700

    SolutionPage draft

--- src/pages/SolutionPage.js
+++ src/pages/SolutionPage.js
@@ -1,6 +1,30 @@
 function SolutionPage(){
     return (
         <>
+        <section className="flex flex-col text-white flex justify-between items-center max-w-[1240px] mx-auto px-4"> 
+        <h1 className="">Services </h1>
+            <p className="">The core focus of our company is offering your company a Customized Finance Team (CFT), starting with fractional CFOs. However, we also offer finance workshops that we tailor to your company.</p>
+            <p> Customized Finance Team</p>
+            <p> We meet with your team to understand your finance needs. From there, we work with our roster to propose your CFT. The primary benefit is you are able to obtain greater specialized expertise, as needed, while ensuring your reporting is delivered in a timely and accurate manner. It’s simple, you get more value for the same cost as maintaining an in house team. </p>
+            <p>Workshops </p>
+            <section className="flex flex-row sm:flex-col">
+
+            <div class="block max-w-sm p-6 bg-slate border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p></div>
+
+            <div class="block max-w-sm p-6 bg-slate border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p></div>
+
+            <div class="block max-w-sm p-6 bg-slate border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p></div>
+
+
+            </section>
+        
+        </section>
         
         </>
     );
