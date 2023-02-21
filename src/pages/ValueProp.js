c176f39ff0943f2a3cce4c84d2173a01723e3e38
Dylan L <dylancblarocque@gmail.com>
2023-02-20 22:45:25 -0700

    ValuePage draft

--- src/pages/ValueProp.js
+++ src/pages/ValueProp.js
@@ -1,6 +1,7 @@
 function ValueProp(){
     return (
-        <>
+        <div className="xl: max-w-[1240px] w-full mx-auto text-center flex items-center flex-col justify-center text-white">
+            <h2 className=""> Purpose</h2>
         <p> Why: Small and medium-sized businesses in Alberta are struggling to manage their finances due to a lack of access to professional finance services, while at the same time, some businesses are facing the challenge of bloated accounting teams, which can lead to decreased capital efficiency and lower profitability. </p>
         
         <p>What?: We offer a unique ecosystem that provides access to a team of highly engaged and knowledgeable finance professionals. Our team works with businesses to provide customized solutions that meet their unique needs, ensuring that their financials are always on track. </p>
@@ -9,7 +10,7 @@ function ValueProp(){
         
 
         <p> Whether you need help with bookkeeping, tax planning, or financial reporting, our team has the expertise and experience to deliver results. We pride ourselves on providing excellent customer service and building long-term relationships with our clients. When you work with us, you can trust that your business's financial health is in good hands.</p>
-        </>
+        </div>
     );
 };
 
