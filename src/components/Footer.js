fcce722fb27794521fd18f4a6a940c8b1d1e6d78
Dylan L <dylancblarocque@gmail.com>
2023-02-20 18:40:39 -0700

    Move footer to bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 function Footer(){
     return (
-        <div className="flex justify-between row-auto max-width-[1240px] mx-auto mt-4 py-8 px-4 text-slate bg-white">
+        <div className="flex justify-between row-auto max-width-[1240px] mx-auto mt-4 py-6 px-4 text-slate bg-white fixed inset-x-0 bottom-0">
         <a href="mailto: justin.l@rocadvisors.ca"> justin.l@rocadvisors.ca</a>
         <p> &copy; 2023 Roc Advisors </p>
         <a href="tel: 780-691-5704"> 780-691-5704</a>
